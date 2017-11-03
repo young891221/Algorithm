d92c1baaa12d03e793f5df1ece60ecc6a5c4568c
young891221 <young891221@gmail.com>
2017-11-03 18:55:51 +0900

    개선

--- src/beakjoon/dp/T2579_UpStairs_DP.java
+++ src/beakjoon/dp/T2579_UpStairs_DP.java
@@ -1,10 +1,8 @@
 package beakjoon.dp;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 
 /**
  * Created by Administrator on 2017-06-20.
@@ -22,39 +20,33 @@ import java.util.Scanner;
 100
  */
 public class T2579_UpStairs_DP {
-    public static void main(String[] args) throws FileNotFoundException {
-        InputStream in = new FileInputStream("C:/Users/Kim YJ/Desktop/2579.txt");
-        Scanner scanner = new Scanner(new InputStreamReader(in));
-        //Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
         int a[] = new int[n+2];
-        int d[][] = new int[n+2][3];
+        int d[][] = new int[n+2][2];
 
         for(int i = n-1; i >= 0; i--) {
-            a[i] = scanner.nextInt();
+            a[i] = Integer.parseInt(br.readLine());
         }
 
-        int result = dp(n, a, d);
+        dp(n, a, d);
+    }
+
+    private static void dp(int n, int[] a, int[][] d) {
+        int result = 0;
+        d[0][0] = a[0];
+        d[1][1] = d[0][0] + a[1];
+        d[2][0] = d[0][0] + a[2];
+        for(int i = 2; i < n; i++) {
+            d[i][0] = getMax(d[i-2]) + a[i];
+            d[i][1] = d[i-1][0] + a[i];
+            result = result < getMax(d[i]) ? getMax(d[i]) : result; //거꾸로 수행하기에 꼭 마지막을 밟는게 아니므로 맥스 캐시값이 필요
+        }
         System.out.println(result);
     }
 
-    private static int dp(int n, int[] a, int[][] d) {
-        int result = 0;
-        d[0][1] = a[0];
-        d[1][2] = d[0][1] + a[1];
-        d[2][1] = d[0][1] + a[2];
-        for(int i = 2; i < n; i++) {
-            for(int j = 1; j < 3; j++) {
-                if(j == 1) {
-                    int key = d[i-2][1] > d[i-2][2] ? 1 : 2;
-                    d[i][1] = d[i-2][key] + a[i];
-                }
-                if(j == 2) {
-                    d[i][2] = d[i-1][1] + a[i];
-                }
-                result = result < d[i][j] ? d[i][j] : result;
-            }
-        }
-        return result;
+    private static int getMax(int[] ints) {
+        return Math.max(ints[0], ints[1]);
     }
 }
