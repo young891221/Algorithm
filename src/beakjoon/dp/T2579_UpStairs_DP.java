87d1c28083c1e3151d7a78c1a60051d44ee9daeb
young891221 <young891221@gmail.com>
2017-11-03 18:15:33 +0900

    .

--- src/beakjoon/dp/T2579_UpStairs_DP.java
+++ src/beakjoon/dp/T2579_UpStairs_DP.java
@@ -1,12 +1,16 @@
 package beakjoon.dp;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 /**
  * Created by Administrator on 2017-06-20.
  * 계단 연속 세개 x, 한 번에 한 계단/두 계단, 첫 계단은 안밟아도 되지만 마지막은 꼭 밟아야 된다
- * d[i] = i까지 최대 합 = 거꾸로 마지막 계단부터 시작해서 내려가는 최대 합을 구해도 된다
- * 거꾸로 = if(a[i+1] > a[i+2]) d[i+1] = d[i+1] + a[i], i+= 1
+ * d[i][j] = i: 계단 순서, j: 몇번 연속인지
+ * i까지 최대 합 = 거꾸로 마지막 계단부터 시작해서 내려가는 최대 합을 구해도 된다
 8
 100
 100
@@ -18,32 +22,38 @@ import java.util.Scanner;
 100
  */
 public class T2579_UpStairs_DP {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
+    public static void main(String[] args) throws FileNotFoundException {
+        InputStream in = new FileInputStream("C:/Users/Kim YJ/Desktop/2579.txt");
+        Scanner scanner = new Scanner(new InputStreamReader(in));
+        //Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
         int a[] = new int[n+2];
-        int d[] = new int[n+2];
+        int d[][] = new int[n+2][3];
 
         for(int i = n-1; i >= 0; i--) {
-            a[i] = scan.nextInt();
+            a[i] = scanner.nextInt();
         }
 
-        int result = wrongDp(n, a, d);
-
+        int result = dp(n, a, d);
         System.out.println(result);
     }
 
-    //이전값을 갖고는 있지만 끝 지점에 따라 이전 노드의 최대값이 달라질 수 있으므로 틀린 로직
-    private static int wrongDp(int n, int[] a, int[] d) {
+    private static int dp(int n, int[] a, int[][] d) {
         int result = 0;
-        d[0] = a[0];
-        for(int i = 0; i < n;) {
-            if(i == n-1) break;
-            d[i+1] = d[i+1] > 0 ? d[i+1] : a[i+1] + d[i];
-            d[i+2] = d[i+2] > 0 ? d[i+2] : a[i+2] + d[i];
-            if(a[i+1] > a[i+2]) i += 1;
-            else i += 2;
-            result = result < d[i] ? d[i] : result;
+        d[0][1] = a[0];
+        d[1][2] = d[0][1] + a[1];
+        d[2][1] = d[0][1] + a[2];
+        for(int i = 2; i < n; i++) {
+            for(int j = 1; j < 3; j++) {
+                if(j == 1) {
+                    int key = d[i-2][1] > d[i-2][2] ? 1 : 2;
+                    d[i][1] = d[i-2][key] + a[i];
+                }
+                if(j == 2) {
+                    d[i][2] = d[i-1][1] + a[i];
+                }
+                result = result < d[i][j] ? d[i][j] : result;
+            }
         }
         return result;
     }
