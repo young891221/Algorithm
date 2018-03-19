a23ac6043082512b45296d044602eebbed900ee5
young891221 <young891221@gmail.com>
2018-03-19 23:47:19 +0900

    팩토리얼

---
+++ src/study/팩토리얼.java
@@ -0,0 +1,13 @@
+public class 팩토리얼 {
+    public static void main(String[] args) {
+        int input = 4;
+        System.out.println(fact(input));
+    }
+
+    private static int fact(int input) {
+        if(input <= 1)
+            return input;
+        else
+            return fact(input-1) * input;
+    }
+}
