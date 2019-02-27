eb111f85473fc11cef1ea1c75b2efc0d1f7908f2
young891221 <young891221@gmail.com>
2019-02-27 09:20:12 +0900

    .

--- src/leetcode/Median_of_Two_Sorted_Arrays.java
+++ src/leetcode/Median_of_Two_Sorted_Arrays.java
@@ -11,23 +11,24 @@ import java.util.Arrays;
 public class Median_of_Two_Sorted_Arrays {
 
     public static void main(String[] args) {
-        double[] num1 = new double[]{1,3,7};
-        double[] num2 = new double[]{3};
+        int[] num1 = new int[]{3};
+        int[] num2 = new int[]{-2,-1};
 
         System.out.println(solution(num1, num2));
     }
 
-    private static String solution(double[] num1, double[] num2) {
+    private static double solution(int[] num1, int[] num2) {
         double midNum1 = calMid(num1);
         double midNum2 = calMid(num2);
 
-        return String.valueOf((midNum1 + midNum2) / 2);
+        int index = num1.length == 0 || num2.length == 0 ? 1 : 2;
+        return (midNum1 + midNum2) / index;
     }
 
-    private static double calMid(double[] nums) {
+    private static double calMid(int[] nums) {
         if (nums.length == 1) {
             return nums[0];
         }
-        return Arrays.stream(nums).sum() / 2;
+        return Arrays.stream(nums).mapToDouble(i -> i).sum() / 2;
     }
 }
